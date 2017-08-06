3ee82ad4a60f4f4e5013d65a12d2206d41fb1294
srjcnathan73 <nwsebastopol@hotmail.com>
2017-08-05 17:04:21 -0700

    Add files via upload

--- tankgame/tanks.js
+++ tankgame/tanks.js
@@ -81,12 +81,12 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: p
 
 function preload () {
 
-    game.load.atlas('tank', 'assets//tanks/tanks.png', 'assets/games/tanks/tanks.json');
-    game.load.atlas('enemy', 'assets//tanks/enemy-tanks.png', 'assets/games/tanks/tanks.json');
-    game.load.image('logo', 'assets//tanks/logo.png');
-    game.load.image('bullet', 'assets//tanks/bullet.png');
-    game.load.image('earth', 'assets//tanks/scorched_earth.png');
-    game.load.spritesheet('kaboom', 'assets//tanks/explosion.png', 64, 64, 23);
+    game.load.atlas('tank', 'assets/games/tanks/tanks.png', 'assets/games/tanks/tanks.json');
+    game.load.atlas('enemy', 'assets/games/tanks/enemy-tanks.png', 'assets/games/tanks/tanks.json');
+    game.load.image('logo', 'assets/games/tanks/logo.png');
+    game.load.image('bullet', 'assets/games/tanks/bullet.png');
+    game.load.image('earth', 'assets/games/tanks/scorched_earth.png');
+    game.load.spritesheet('kaboom', 'assets/games/tanks/explosion.png', 64, 64, 23);
     
 }
 
